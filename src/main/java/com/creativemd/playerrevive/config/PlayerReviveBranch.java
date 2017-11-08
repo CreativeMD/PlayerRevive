c61f75ee4510a6a16e0f4ee7f69da8b0eab9fb72
N247S <fictiefverzonnen@gmail.com>
2017-11-08 21:29:59 +0100

    Update PlayerReviveBranch.java

--- src/main/java/com/creativemd/playerrevive/config/PlayerReviveBranch.java
+++ src/main/java/com/creativemd/playerrevive/config/PlayerReviveBranch.java
@@ -5,6 +5,7 @@ import com.creativemd.igcm.api.segments.BooleanSegment;
 import com.creativemd.igcm.api.segments.IntegerSegment;
 import com.creativemd.playerrevive.PlayerRevive;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,18 +15,18 @@ import net.minecraftforge.fml.relauncher.Side;
 public class PlayerReviveBranch extends ConfigBranch {
 
 	public PlayerReviveBranch() {
-		super("Player Revive", new ItemStack(Items.TOTEM));
+		super(I18n.format("playerrevive.config.branch_name"), new ItemStack(Items.TOTEM));
 	}
 
 	@Override
 	public void createChildren() {
-		registerElement("playerReviveTime", new IntegerSegment("Revive Time", 100, 1, Integer.MAX_VALUE));
-		registerElement("playerReviveSurviveTime", new IntegerSegment("Survive Time", 1200, 1, Integer.MAX_VALUE));
+		registerElement("playerReviveTime", new IntegerSegment(I18n.format("playerrevive.config.player_revive_time"), 100, 1, Integer.MAX_VALUE));
+		registerElement("playerReviveSurviveTime", new IntegerSegment(I18n.format("playerrevive.config.player_revive_survive_time"), 1200, 1, Integer.MAX_VALUE));
 		
-		registerElement("playerHealthAfter", new IntegerSegment("Health After", 2, 1, 20));
-		registerElement("playerFoodAfter", new IntegerSegment("Food After", 6, 1, 20));
+		registerElement("playerHealthAfter", new IntegerSegment(I18n.format("playerrevive.config.player_health_after"), 2, 1, 20));
+		registerElement("playerFoodAfter", new IntegerSegment(I18n.format("playerrevive.config.player_food_after"), 6, 1, 20));
 		
-		registerElement("banPlayerAfterDeath", new BooleanSegment("Ban After Death", false));
+		registerElement("banPlayerAfterDeath", new BooleanSegment(I18n.format("playerrevive.config.ban_player_after_death"), false));
 	}
 
 	@Override
